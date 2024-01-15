7dfb79f6fc82b91898cd55b53212317a7062558d
jindduel <jinsol1533@gmail.com>
2024-01-15 15:43:44 +0900

    delete source

--- JavaTest/JavaStart.java
+++
@@ -1,6 +0,0 @@
-public class JavaStart {
-	public static void main(String [] args){
-		System.out.println("***Java Strat!!!***");
-		System.out.print("안녕 나는 수연샘이야");
-	}
-}
--- JavaTest/MyInfo.java
+++
@@ -1,9 +0,0 @@
-public class MyInfo{
-	public static void main(String [] args){
-		/*System.out.println(" [학생 정보]");
-		System.out.println("전공 : " + args[0] + "학과");
-		System.out.println("학년 : " + args[1] + "학년");*/
-
-		System.out.print("전공은 " + args[0] + "이며 학년은 " + args[1] + "입니다.");
-	}
-}
